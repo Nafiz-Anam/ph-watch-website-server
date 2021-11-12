7e748c38f6d827a853acfc40caac8cbe37c4010f
Nafiz Anam <nafiz.anam002@gmail.com>
2021-11-13 02:36:31 +0600

    fix

--- index.js
+++ index.js
@@ -146,21 +146,6 @@ async function run() {
             // console.log("Deleting user with id :", id);
             // console.log("Result :", result);
         });
-
-        // app.get("/appointments", async (req, res) => {
-        //     const email = req.query.email;
-        //     const date = new Date(req.query.date).toLocaleDateString();
-        //     const query = { email: email, date: date };
-        //     const cursor = appointmentsCollection.find(query);
-        //     const appointments = await cursor.toArray();
-        //     res.json(appointments);
-        // });
-        // app.post("/appointments", async (req, res) => {
-        //     const appointment = req.body;
-        //     const result = await appointmentsCollection.insertOne(appointment);
-        //     console.log(result);
-        //     res.json(result);
-        // });
     } finally {
         // await client.close();
     }
